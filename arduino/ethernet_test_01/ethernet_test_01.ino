4f7680c4bdca1817fab27d19bd01e88b80003864
Eoghan Furlong <furlong.eoghan@gmail.com>
2017-07-20 22:53:02 +0100

    removed old test file & updated some variables to correct types

--- arduino/ethernet_test_01/ethernet_test_01.ino
+++ arduino/ethernet_test_01/ethernet_test_01.ino
@@ -8,24 +8,25 @@
                                       Defines 
 *****************************************************************************************************/
 /* relay states - these relays are "active low" */
-#define RELAY_OFF  ( 1 )
-#define RELAY_ON   ( 0 )
+#define RELAY_OFF  ( 1u )
+#define RELAY_ON   ( 0u )
 
 /* arduino I/O pin numbers that relays are connected to */
-#define RELAY_1  ( 30 )
-#define RELAY_2  ( 31 )
-#define RELAY_3  ( 32 )
-#define RELAY_4  ( 33 )
+#define RELAY_1  ( 30u )
+#define RELAY_2  ( 31u )
+#define RELAY_3  ( 32u )
+#define RELAY_4  ( 33u )
 
 /* serial comms parameters */
-#define SERIAL_0_BAUD_RATE    ( 9600 )  /* PC to Arduino via USB */
+#define SERIAL_0_BAUD_RATE    ( 9600u )  /* PC to Arduino via USB */
 
 /* ethernet parameters */
-byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+//TODO: find out why the ethernet libraries don't like these defined as const
+static byte mac_addr_s[] = { 0xDEu, 0xADu, 0xBEu, 0xEFu, 0xFEu, 0xEDu };
 
-IPAddress ip_addr( 192,168,0,116 );
+static IPAddress ip_addr_s( 192u, 168u, 0u, 116u );
 
-#define server_port    ( 80 )    /* default http port */
+#define server_port    ( 80u )    /* default http port */
 
 /***************************************************************************************************** 
                                       Globals 
@@ -37,7 +38,7 @@ static byte   input_byte_s;
 /***************************************************************************************************** 
                                       Initial setup 
 *****************************************************************************************************/
-EthernetServer my_webserver( server_port );
+static EthernetServer my_webserver_s( server_port );
 
 void setup() 
 {
@@ -60,9 +61,9 @@ void setup()
   pinMode( RELAY_4, OUTPUT );
   
   /********** Ethernet setup **********/
-  Ethernet.begin( mac_addr, ip_addr );
+  Ethernet.begin( mac_addr_s, ip_addr_s );
   
-  my_webserver.begin();
+  my_webserver_s.begin();
   
   Serial.print( "Server is at: " );
   Serial.println( Ethernet.localIP() );
@@ -80,15 +81,19 @@ void loop()
   /* Webserver/ethernet */
   ethernet_webserver();
   
-  
 }
 
 /***************************************************************************************************** 
                                  Private functions
 *****************************************************************************************************/
+
+/*********************************************************************************
+*  \brief   Handles webserver connections, requests and responses
+*  \author  A mix of tutorials, with some of my own code to make it a bit safer
+*********************************************************************************/
 void ethernet_webserver( void )
-{
-  EthernetClient client = my_webserver.available();
+{  
+  EthernetClient client = my_webserver_s.available();
   
   if( client )
   {
@@ -98,29 +103,62 @@ void ethernet_webserver( void )
     
     /* a client is considered connected if the connection has been closed but 
     there is still unread data */
+    // TODO: is there a better way to do this than using a while loop? - potentially blocking
     while( client.connected() )
     {
-      if( client.available() )
+      //TODO: client.available returns the number of bytes available - use this info!!
+      //if( client.available() )   /* TODO: look out for this - if(x) - x could be any number > 0 */
+      if( client.available() > 0u )
       {
         char rxd_char = client.read();
         Serial.write( rxd_char );
         
         /* http request ends with a blank line */
+        if( rxd_char == '\n' && current_line_is_blank )
+        {
+           /* send a standard http response header */
+           client.println( "HTTP/1.1 200 OK" );
+           client.println( "Content-Type: text/html" );
+           client.println( "Connection: close" ); /* connection closed when response completes */
+           client.println( "Refresh: 15" );        /* auto-refresh the page every 5 seconds */
+           client.println();
+           
+           /* HTML page in response */
+           client.println( "<!DOCTYPE HTML>" );
+           client.println( "<html>" );
+           
+           client.println( "YOLO" );  //TODO: add something a bit more wholesome to the response :)
+           
+           client.println( "</html>" );
+        }
+        
         if( rxd_char == '\n' )
         {
-           /* send a standard http response header */ 
+          current_line_is_blank = true; 
+        }
+        else if( rxd_char != '\r' )
+        {
+          current_line_is_blank = false; 
         }
       }   
    
     } 
     
-    char c = client.read();
-    Serial.write(c);
+    /* give the web browser time to receive the data */
+    delay( 1u );
+    
+    /* close the connection & notify whoever is listening on the serial port */
+    client.stop();
+    Serial.println( "client disconnected." );
+    
   }
   
 }
 
-
+/*********************************************************************************
+*  \brief   Control of Songle relay blocks using input from Serial port
+*  \author  EFurlong
+*********************************************************************************/
 void serial_relay_control( void )
 {
   if( Serial.available() )
@@ -131,58 +169,59 @@ void serial_relay_control( void )
     /* clear the serial rx buffer - we don't care what comes after first byte */
     Serial.flush();
     
+    /* inputs are ASCII - using numbers 1 - 7 to control relays */
     switch( input_byte_s )
     {
-      case( 0x30 ):
+      case( 0x30u ):
       {
         Serial.write( "Relay 01 OFF. \n" );
         digitalWrite( RELAY_1, RELAY_OFF );
       }
       break;
      
-      case( 0x31 ):
+      case( 0x31u ):
       {
         Serial.write( "Relay 01 ON. \n" );
         digitalWrite( RELAY_1, RELAY_ON );
       }
       break;
     
-      case( 0x32 ):
+      case( 0x32u ):
       {
         Serial.write( "Relay 02 OFF. \n" );
         digitalWrite( RELAY_2, RELAY_OFF );
       }
       break;
       
-      case( 0x33 ):
+      case( 0x33u ):
       {
         Serial.write( "Relay 02 ON. \n" );
         digitalWrite( RELAY_2, RELAY_ON );
       }
       break;
     
-      case( 0x34 ):
+      case( 0x34u ):
       {
         Serial.write( "Relay 03 OFF. \n" );
         digitalWrite( RELAY_3, RELAY_OFF );
       }
       break;
       
-      case( 0x35 ):
+      case( 0x35u ):
       {
         Serial.write( "Relay 03 ON. \n" );
         digitalWrite( RELAY_3, RELAY_ON );
       }
       break;
     
-      case( 0x36 ):
+      case( 0x36u ):
       {
         Serial.write( "Relay 04 OFF. \n" );
         digitalWrite( RELAY_4, RELAY_OFF );
       }
       break;
       
-      case( 0x37 ):
+      case( 0x37u ):
       {
         Serial.write( "Relay 04 ON. \n" );
         digitalWrite( RELAY_4, RELAY_ON );
